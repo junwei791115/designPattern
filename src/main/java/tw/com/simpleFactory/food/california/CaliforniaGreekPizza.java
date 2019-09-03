8fb5f7b46cc4271fff5c4002fe444220b48fd058
junwei.chen <JUNWEI.CHNE@DELTAWW.COM>
2019-09-03 17:07:38 +0800

    commit

---
+++ src/main/java/tw/com/simpleFactory/food/california/CaliforniaGreekPizza.java
@@ -0,0 +1,8 @@
+package tw.com.simpleFactory.food.california;
+
+import tw.com.simpleFactory.food.Pizza;
+
+public class CaliforniaGreekPizza extends Pizza {
+
+
+}
---
+++ src/main/java/tw/com/simpleFactory/food/california/CaliforniaPepperoniPizza.java
@@ -0,0 +1,8 @@
+package tw.com.simpleFactory.food.california;
+
+import tw.com.simpleFactory.food.Pizza;
+
+public class CaliforniaPepperoniPizza extends Pizza {
+
+
+}
